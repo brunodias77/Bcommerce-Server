d9f1e55dec8a904665607f8382b16c08841475f0
brunodias77 <brunohdias95@gmail.com>
2025-10-26 13:10:14 -0700

    feat: iniciando o catalog-service

--- Src/BuildingBlocks/BuildingBlocks/MessageBus/Command.cs
+++
@@ -1,8 +0,0 @@
-namespace BuildingBlocks.MessageBus;
-
-public abstract record Command : ICommand
-{
-    public Guid MessageId { get; init; } = Guid.NewGuid();
-    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
-    public abstract string CommandName { get; }
-}
--- Src/BuildingBlocks/BuildingBlocks/MessageBus/IMessageHandler.cs
+++
@@ -1,9 +0,0 @@
-namespace BuildingBlocks.MessageBus;
-
-/// <summary>
-/// Handler único para processar mensagens
-/// </summary>
-public interface IMessageHandler<in TMessage> where TMessage : IMessage
-{
-    Task HandleAsync(TMessage message, CancellationToken cancellationToken = default);
-}
